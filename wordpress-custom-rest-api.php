6985a70e6d2905589a833e225db7825b5fc35246
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2022-09-16 17:44:07 +0530

    updated

--- wordpress-custom-rest-api.php
+++ wordpress-custom-rest-api.php
@@ -8,3 +8,33 @@
  * Author: Mr. Kahnu
  * Author URI: https://developernoob.in
  */
+
+
+add_action('rest_api_init', function () {
+    register_rest_route( 'public/v1', 'all-users',array(
+                  'methods'  => 'GET',
+                  'callback' => 'get_latest_posts_by_category'
+        ));
+  });
+
+
+function get_latest_posts_by_category($request)
+{
+
+
+    global $wpdb;
+    $wp_users = $wpdb->prefix . "users";
+
+
+    $allUsers = $wpdb->get_results("SELECT * FROM $wp_users ");
+
+    $response = array(
+        "success" => true,
+        "data"=> $allUsers
+    );
+
+    $response = new WP_REST_Response($response );
+    $response->set_status(200);
+
+    return $response;
+}
