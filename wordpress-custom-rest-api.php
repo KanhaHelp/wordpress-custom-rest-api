1abefb855e8ae6c0a3bef4eb159894c097bf0344
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2022-09-16 16:52:20 +0530

    first commit

---
+++ wordpress-custom-rest-api.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Wordpress Custom REST API
+ * Plugin URI: https://developernoob.in
+ * Description: This plugin provide a REST API for all wordpress user
+ * Version: 1.0
+ * Author: Mr. Kahnu
+ * Author URI: https://developernoob.in
+ */
